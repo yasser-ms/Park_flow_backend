68b0449b46cf57b7fb5ffd021069a93ae9863eac
Yasser <yasseretudes004@gmail.com>
2026-01-09 17:59:23 +0100

    Fix cookie settings for cross-domain

--- app/config.py
+++ app/config.py
@@ -25,8 +25,8 @@ class Config:
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_COOKIE_SECURE = False  # True en prod HTTPS
-    JWT_COOKIE_SAMESITE = "Lax"
+    JWT_COOKIE_SECURE = True
+    JWT_COOKIE_SAMESITE = "None"
 
     JWT_COOKIE_CSRF_PROTECT = True
     JWT_CSRF_IN_COOKIES = True
