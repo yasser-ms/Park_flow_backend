174608742382b7fcbf5baf918a002ba0cd34af2c
Yasser <yasseretudes004@gmail.com>
2026-01-09 18:12:42 +0100

    Fix cookie settings for cross-domain

--- app/config.py
+++ app/config.py
@@ -27,6 +27,10 @@ class Config:
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_COOKIE_SECURE = True
     JWT_COOKIE_SAMESITE = "None"
+    
+    JWT_COOKIE_DOMAIN = None  
+    JWT_ACCESS_COOKIE_PATH = "/"  
+    JWT_REFRESH_COOKIE_PATH = "/"  
 
     JWT_COOKIE_CSRF_PROTECT = True
     JWT_CSRF_IN_COOKIES = True
